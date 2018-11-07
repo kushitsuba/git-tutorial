52abea659fb467010b126690c52e32b223439177
kushibiki <j1516@matsue-ct.jp>
2018-11-07 11:09:36 +0900

    Add feature-newRB

--- kadaij1516.rb
+++ kadaij1516.rb
@@ -1,8 +1,12 @@
 def fizzbuzz(num)
   return :FizzBuzz if num % 15 == 0
+  return :git if num %7 == 0
   return :Buzz if num % 5 == 0
   return :Fizz if num % 3 == 0
 
   num
 end
 
+(1..100).map{ |num| fizzbuzz(num) }.each do |num|
+  puts num
+end
